a3a5f1acd4d99d765ad50847b753e7250067dd50
Anupam <106304435+Anupam-Bh@users.noreply.github.com>
2025-11-11 12:40:51 +0000

    Delete Trivialcat_v4 directory

--- Trivialcat_v4/clear_vasp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rm -f AEC* ELFCAR output LOCPOT  CHG CHGCAR CONTCAR STOPCAR DOSCAR DYNMAT EIGENVAL IBZKPT OPTIC OSZICAR OUTCAR PROCAR PCDAT WAVECAR XDATCAR PARCHG vasprun.xml REPORT wannier90_band.gnu wannier90_band.kpt wannier90.chk wannier90.wout vaspout.h5 PENALTYPOT HILLSPOT ML_LOGFILE ML_ABN ML_FFN ML_HIS ML_REG  *.amn *.mmn *.eig wannier90.win
